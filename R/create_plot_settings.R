1c8f6ea99f68608be0a3270f3e0d3a5d93f09b07
kel-github <getkellygarner@gmail.com>
2021-07-21 21:42:21 +1000

    made decision on z tests

--- R/create_plot_settings.R
+++ R/create_plot_settings.R
@@ -9,32 +9,32 @@ sub_Ns <- paste(round(exp(seq(log(13), log(313), length.out = 20))))
 # density variables
 # ----------------------------------------------------
 fx <- list(datpath = "../data/",
-           task = "CC",
+           task = task,
            jmax = 2,
            dv = "dens_fx",
            sel_n = paste(c(25, 59, 136, 313)),
            w = 1.96,
            h = 2.36 * 2,
            xlabs = c(expression(eta[p]^2), expression("r"^2)),
-           xl = c(0, 0.6),
+           xl = c(0, 0.4),
            max_idx = c(20, 20),
            leg_id = 2,
-           leg_locs = c(0.3, 300),
+           leg_locs = c(0.2, 300),
            figlabel = "B",
            figlabelon = TRUE)
 
 p <- list(datpath = "../data/",
-          task = "CC",
+          task = task,
           jmax = 2,
           dv = "dens_p",
           sel_n = paste(c(25, 59, 136, 313)),
           w = 1.96,
           h = 2.36 * 2,
           xlabs = c("p", "p"),
-          xl = c(-15, 10),
-          max_idx = c(5, 20),
+          xl = c(-35, 10),
+          max_idx = c(20, 20),
           leg_id = 1,
-          leg_locs = c(-60, 0.4),
+          leg_locs = c(-35, 0.4),
           figlabel = "A",
           figlabelon = TRUE)
 
@@ -43,7 +43,7 @@ p <- list(datpath = "../data/",
 # ----------------------------------------------------
 
 kl <- list(datpath = "../data/",
-           task = "CC",
+           task = task,
            dv = "dens_fx",
            ratio_type = "KL",
            origin = "313",
@@ -61,7 +61,7 @@ kl <- list(datpath = "../data/",
 # ----------------------------------------------------
            
 model_rats <- list(datpath = "../data/",
-              task = "CC",
+              task = task,
               dv = "stats_fx",
               ratio_type = "model",
               origin = "",
@@ -79,23 +79,23 @@ model_rats <- list(datpath = "../data/",
 # meta-analytic vs observed mus
 # ----------------------------------------------------
 meta_mu <- list(datpath = "../data/",
-                task = "CC",
+                task = task,
                 mods = c("RM-AN", "LME"),
                 sub_Ns = paste(round(exp(seq(log(13), log(313), length.out = 20)))),
-                yl = c(-0.2, .2),
-                leg_locs = c(2, .2),
+                yl = c(-0.15, .15),
+                leg_locs = c(2, .145),
                 leg_id = TRUE,
-                sig_lines = list("RM-AN" = list(c(1,19)), "LME" = list(c(1,20))),
-                sig_y = c(-.18, -.15))
+                sig_lines = NULL,
+                sig_y = NULL)
 
 # ----------------------------------------------------
 # model mu difference
 # ----------------------------------------------------
 model_mu_diff <- list(datpath = "../data/",
-                      task = "CC",
+                      task = task,
                       mods = c("RM-AN", "LME"),
                       sub_Ns = paste(round(exp(seq(log(13), log(313), length.out = 20)))),
-                      yl = c(-0.1, .2),
+                      yl = c(-0.05, .2),
                       leg_locs = NULL,
                       leg_id = FALSE,
                       sig_lines = c(1,20),
@@ -106,7 +106,7 @@ model_mu_diff <- list(datpath = "../data/",
 # meta-analytic vs observed fx sz ratio
 # ----------------------------------------------------
 sig <- list(datpath = "../data/",
-            task = "CC",
+            task = task,
             dv = "stats_sig",
             ratio_type = "stats_sig",
             origin = "",
