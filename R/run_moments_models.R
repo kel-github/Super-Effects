697133b7d541faa460c538a1548be6c38046541b
kelly <getkellygarner@gmail.com>
2023-07-20 16:48:06 +1000

    added printing and quitting to moments models

--- R/run_moments_models.R
+++ R/run_moments_models.R
@@ -29,15 +29,20 @@ zipnms <- c("SD_wv.zip", "EPSAB_wv.zip", "SRT_wv.zip")
 sub.Ns <- round(exp(seq(log(13), log(313), length.out = 20)))
 maxN = 313
 
+print("running AB models")
 run_models(f=run_AB_models, datpath=datpath, task="AB", zipnm="EPSAB_wv.zip",
            maxN=313, Ns=sub.Ns)
 
+print("running SRT models")
 run_models(f=run_SRT_models, datpath=datpath, task="SRT", zipnm="SRT_wv.zip",
            maxN=313, Ns=sub.Ns)
 
+print("running SD ME models")
 run_models(f=run_SD_ME, datpath=datpath, task="SD", zipnm="SD_wv.zip",
            maxN=313, Ns=sub.Ns)
 
+print("running SD int models")
 run_models(f=run_SD_int, datpath=datpath, task="SD", zipnm="SD_wv.zip",
            maxN=313, Ns=sub.Ns)
 
+quit()
