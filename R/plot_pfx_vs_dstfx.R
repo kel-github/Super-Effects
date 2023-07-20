08f6cf9522fb8c964f65fa6830eccbffbdfe9d54
kelly <getkellygarner@gmail.com>
2023-07-20 14:10:07 +1000

    AB and SRT done

--- R/plot_pfx_vs_dstfx.R
+++ R/plot_pfx_vs_dstfx.R
@@ -145,13 +145,22 @@ do_stp_n_prs <- function(df, dv, task, datpath = "../data/", prs_fnm, rsd_fm){
   pdf(paste(datpath, task, paste(rsd_fm, "resid.pdf", sep="_"), sep="/"))
   plot(step_model, which = c(1, 2))
   dev.off()
+  
+  pdf(paste(datpath, task, paste(rsd_fm, "aV.pdf", sep="_"), sep="/"))
+  avPlots(step_model)
+  dev.off()
+
   list(vif_out, step_model)
 }
 
+r2z <- function(r){
+  # Fisher R to Z transform https://en.wikipedia.org/wiki/Fisher_transformation
+  0.5*log((1+r)/(1-r))
+}
 ################################################################################
 # Run things
 ################################################################################
-
+not_new <- T
 # define sub Ns
 datpath <- "../data"
 tasks <- c("SD", "AB", "SRT")
@@ -162,6 +171,7 @@ tstN = 25
 Ns <- c(tstN, maxN) # for initial testing
 
 # first get a list, where each element is the data from one task
+if (!not_new){ 
 dat <- mapply(function(x,y, Ns) do.call(rbind, lapply(Ns, 
                                                   get_dat_4corrs, 
                                                   datpath=datpath, 
@@ -172,6 +182,9 @@ dat <- mapply(function(x,y, Ns) do.call(rbind, lapply(Ns,
                 MoreArgs = list(Ns = Ns))
 names(dat) <- tasks
 save(dat, file = paste(datpath, "fx_by_dst_regression.RData", sep="/"))
+} else {
+  load(paste(datpath, "fx_by_dst_regression.RData", sep="/"))
+}
 ##############
 # now for each task, compute the appropriate distance variables
 ##############################################################
@@ -206,72 +219,102 @@ dat[["SRT"]]$esz_dist <- dat[["SRT"]]$esz - mu
 #############################################################
 # AB
 #############################################################
-AB_init_model <- do_stp_n_prs(df = dat[["AB"]] %>% filter(n == tstN) %>%
-                                     select(esz_dist, AB.mu, AB.sigma, AB.skew,
-                                            AB.k, AB.r, sigma_mu),
+AB_model <- do_stp_n_prs(df = dat[["AB"]] %>% filter(n == tstN) %>%
+                                     select(esz_dist, AB.skew,
+                                            AB.k, AB.r, sigma_mu, k_mu),
                               dv = "esz_dist",
                               task = "AB",
                               prs_fnm = "AB",
                               rsd_fm = "AB_init")
-AB_init_model[[1]] # sigma and skew_mu are an issue, but residuals looks ok
+AB_model[[1]] # all good
 # gonna keep sigma mu (correlates highly with skew_mu and k_mu)
 # initial model VIF
-# AB.mu  AB.sigma   AB.skew      AB.k      AB.r  sigma_mu   skew_mu      k_mu 
-# 2.360360  2.252898  2.173843  1.976478  1.669528  7.360513 11.926566  5.276604
-# AB.mu AB.sigma  AB.skew     AB.k     AB.r sigma_mu 
-# 2.140984 1.465127 1.929311 1.866657 1.185319 1.939769
+# AB.skew     AB.k     AB.r sigma_mu  skew_mu     k_mu
+# 1.745574 1.602303 1.236187 5.184596 8.904735 3.439406
+# AB.skew     AB.k     AB.r sigma_mu  skew_mu 
+# 1.726115 1.601608 1.117864 4.569866 4.266436 
+# AB.skew     AB.k     AB.r sigma_mu     k_mu # keeping k_mu as VIF lower
+# 1.733866 1.601263 1.096272 1.785786 1.647888 
 # at this point, residuals look good!
-summary(AB_init_model[[2]]) 
+summary(AB_model[[2]]) 
 # Coefficients:
-#   Estimate Std. Error t value Pr(>|t|)    
+#   Estimate Std. Error t value Pr(>|t|)
 # (Intercept) -0.225060   0.018058 -12.463  < 2e-16 ***
 #   AB.mu        1.689350   0.040806  41.400  < 2e-16 ***
 #   AB.sigma    -6.038839   0.173207 -34.865  < 2e-16 ***
-#   AB.r        -0.027334   0.009056  -3.018  0.00261 ** 
-#   sigma_mu    -0.002633   0.001090  -2.415  0.01592 *  
+#   AB.r        -0.027334   0.009056  -3.018  0.00261 **
+#   sigma_mu    -0.002633   0.001090  -2.415  0.01592 *
 #   ---
 #   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 # 
 # Residual standard error: 0.02954 on 995 degrees of freedom
-# Multiple R-squared:  0.7725,	Adjusted R-squared:  0.7716 
+# Multiple R-squared:  0.7725,	Adjusted R-squared:  0.7716
 # F-statistic: 844.5 on 4 and 995 DF,  p-value: < 2.2e-16
 
-# for mu and sigma of Xdiff
-# apply this over tasks
-#############
 #############################################################
 # SRT
 #############################################################
-SRT_init_model <- do_stp_n_prs(df = dat[["SRT"]] %>% filter(n == tstN) %>%
-                                      select(esz_dist, SRT.mu, SRT.sigma, SRT.skew,
-                                             SRT.k, SRT.r, sigma_mu, skew_mu),
-                                    dv = "esz_dist",
-                                    task = "SRT",
-                                    prs_fnm = "SRT",
-                                    rsd_fm = "SRT")
-SRT_init_model[[1]]
-# skew mu and k_mu a problem. 
-# SRT.mu SRT.sigma  SRT.skew     SRT.k     SRT.r  sigma_mu   skew_mu      k_mu 
-# 1.766901  2.639953  2.839966  2.581512  2.039296  2.182733  7.050590  6.510980 
-# correlation skew_mu & k_mu = 0.914, ditching k_mu
-# second model
-#    SRT.mu SRT.sigma  SRT.skew     SRT.k     SRT.r  sigma_mu   skew_mu 
-# 1.753563  2.616690  2.834695  2.568372  2.039295  2.168606  1.296429 
-# residuals show we're mis-fitting high and low values, will take a 
-# look at all histograms
-dat[["SRT"]] %>% filter(n == tstN) %>% 
-                  select(esz_dist, SRT.mu, SRT.sigma, SRT.skew,
-                          SRT.k, SRT.r, sigma_mu, skew_mu) %>%
-                  pivot_longer(cols = everything(), names_to = "meas", values_to = "values") %>%
-                    ggplot(aes(x=values)) + geom_histogram() + facet_wrap(~meas, scales = "free")
-# sigma_mu, skew_mu, SRT.k and SRT.sigma are positively skewed
-# SRT.r is negatively skewed
-# decide re: transforms - see if that will help
-# https://rcompanion.org/handbook/I_12.html
+# SRT is a little more complex, got some work to do on the variables
+# first
+# 1) get EV of distribution
+get_ev_non_norm(dat[["SRT"]]$esz_dist) #[1] 0.05454278
+# 2) allocate data to a df and make variables behave
+SRT_df <- dat[["SRT"]] %>% filter(n == tstN) %>%
+                            select(esz_dist, SRT.mu, SRT.sigma, SRT.skew,
+                                    SRT.k, SRT.r, sigma_mu, skew_mu, k_mu)
+ggpairs(SRT_df)
+hist(SRT_df$esz_dist, breaks = 30) # positively skewed
+apply(SRT_df %>% select(SRT.k, sigma_mu, skew_mu, k_mu), 2, min) # all +ve 
+# first make all the values positive and log transform
+x = 0.01 - min(SRT_df$esz_dist)
+SRT_df <- SRT_df %>% mutate(esz_dist_t = log(esz_dist + x),
+                            SRT.r_t = scale(r2z(SRT.r), scale=F),
+                            SRT.sigma_t = log(SRT.sigma), # assumes pwr function
+                            SRT.k_t = log(SRT.k), # same
+                            sigma_mu_t = log(sigma_mu), # same
+                            skew_mu_t = log(skew_mu),
+                            k_mu_t = log(k_mu)) # same
 
-# perform the linear regression at each level of N, and take the
-# beta co-efficients for the key features for each task
-# do a correlation between N and beta, as well as plotting (each
-# feature is a panel, each line is a task)
-# what general/conclusive statements can be made?
+hist(SRT_df$esz_dist_t, breaks = 30) # this has squashed in the upper tail, some 
+# large negative values
+ggpairs(SRT_df %>% select(esz_dist_t, SRT.sigma_t, SRT.skew,
+                          SRT.k_t, SRT.r_t, sigma_mu_t, k_mu_t))
+# clearly skewy variables are SRT sigma, SRT.k, sigma_mu, skew_mu
 
+#SRT_df <- SRT_df %>% select(esz_dist_t, SRT.sigma, SRT.k, sigma_mu, skew_mu)
+SRT_df <- SRT_df %>% select(esz_dist_t, SRT.skew, SRT.k_t, SRT.r_t,
+                            sigma_mu_t, k_mu_t)
+SRT_model <- do_stp_n_prs(df = SRT_df,
+                          dv = "esz_dist_t",
+                          task = "SRT",
+                          prs_fnm = "SRT",
+                          rsd_fm = "SRT")
+SRT_model[[1]]
+# SRT.skew    SRT.k_t    SRT.r_t sigma_mu_t  skew_mu_t     k_mu_t 
+# 2.650863   2.394736   1.824746   1.709689   4.147821   4.335670 
+# with k_mu removed
+# SRT.skew    SRT.k_t    SRT.r_t sigma_mu_t  skew_mu_t 
+# 2.650330   2.393024   1.748995   1.609469   1.293854 
+# with skew mu removed
+# SRT.skew    SRT.k_t    SRT.r_t sigma_mu_t     k_mu_t # going with this one as 
+# 2.605319   2.354499   1.823450   1.706594   1.352451 # did w AB and much of a much
+summary(SRT_model[[2]])
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept) -4.68609    0.23587 -19.867  < 2e-16 ***
+#   SRT.skew    -0.07786    0.02580  -3.017  0.00261 ** 
+#   SRT.k_t     -0.08978    0.04310  -2.083  0.03751 *  
+#   SRT.r_t      0.50777    0.03797  13.371  < 2e-16 ***
+#   sigma_mu_t  -1.00907    0.04927 -20.481  < 2e-16 ***
+#   ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Residual standard error: 0.3468 on 995 degrees of freedom
+# Multiple R-squared:  0.3415,	Adjusted R-squared:  0.3389 
+# F-statistic:   129 on 4 and 995 DF,  p-value: < 2.2e-16
+
+#############################################################
+# SD
+#############################################################
+# first look at data for ME and int effects, transform variables
+# to make them behave well
